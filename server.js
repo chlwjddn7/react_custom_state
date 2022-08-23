14d9fd19fb846733612bb4df4b56764a1668ae65
jwchoi <jwchoi@takensoft.co.kr>
2022-08-24 03:17:10 +0900

    220824 최정우 최초커밋

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const server = express();
+
+server.listen(80, function () {
+    console.log('Node.js를 활용한 Web Server 구동(Port:80)');
+})
+
+server.get('/', function(request, response) {
+    //response.sendFile(__dirname + `/views/index.html`);
+    var ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress 
+    console.log('ROOT URL(/)', request.params, ', IP:', ip);
+    response.redirect('/index.html');
+})
+
+server.get('/*.html', function(request, response) {
+    var ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress 
+    console.log('/*.html', request.params, ', IP:', ip);
+    response.sendFile(__dirname + `/views/${request.params['0']}.html`);
+})
+
+server.get('/resources/*', function(request, response) {
+    /* console.log('');
+    console.log('------------------- resources ------------------');
+    console.log('/resources/*.(js|css)', request.params); */
+    response.sendFile(__dirname + `/resources/${request.params['0']}`);
+})
